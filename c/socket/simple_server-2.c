482e1b13afebd1b8ccf66ae27c168cd829b80645
Neel Chauhan <neel@neelc.org>
2016-06-20 10:36:39 -0400

    Add server with input

---
+++ c/socket/simple_server-2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <signal.h>
+
+const int REUSEADDR = 1;
+
+int main(int argc, char *argv[]) {
+	int serv_sock, client_sock;
+	struct sockaddr_in serv_addr;
+
+	serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&serv_addr, '0', sizeof(serv_addr));
+
+	setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &REUSEADDR, sizeof(REUSEADDR));
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(8888);
+
+	bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	listen(serv_sock, 0);
+
+	while (1) {
+		client_sock = accept(serv_sock, (struct sockaddr*)NULL, NULL);
+		write(client_sock, "Hi!\n", 4);
+		char buffer[1024];
+		int size = recv(client_sock, buffer, 512, 0);
+		buffer[size] = '\0';
+		write(client_sock, buffer, size);
+		close(client_sock);
+	}
+
+	close(serv_sock);
+	return 0;
+}
